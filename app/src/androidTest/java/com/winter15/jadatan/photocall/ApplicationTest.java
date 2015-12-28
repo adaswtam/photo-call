0b8d855768a50ad581c3f9a6692182c51164df9a
Ada Tam <adatam@tests-MacBook-Pro.local>
2015-12-28 20:12:42 +0800

    first commit

---
+++ app/src/androidTest/java/com/winter15/jadatan/photocall/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.winter15.jadatan.photocall;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
